62ad2b1532ba59332a1347fb538ed983841f5c16
Armin Rafieyan <arafi4@illinois.edu>
2021-11-07 23:42:15 -0600

    Added monster moving code

--- Project/timer.js
+++ Project/timer.js
@@ -1,14 +1,37 @@
-let startTime
+let startTime;
 function timer() {
-    timerElement.innerText = 0
-    startTime = new Date()
+    timerElement.innerText = 0;
+    startTime = new Date();
     setInterval(() => {
-        getTimerTime()
+        getTimerTime();
     }, 1000)
 }
 
 function getTimerTime() {
-    return Math.floor((new Date() - startTime) / 1000)
+    return Math.floor((new Date() - startTime) / 1000);
 }
 
 renderNewQuote()
+var monster=document.getElementById('monster');
+var game=game.getElementById('game');
+
+var monsterLeft = 0;
+
+function monsterMove(e){
+
+    if(e.keyCode==39){
+        monsterLeft += 2;
+        monster.style.left = monsterLeft + 'px';
+
+    }
+    if(e.keycode==37){
+        monsterLeft -= 2;
+        monster.style.left = monsterLeft + 'px';
+        if(monsterLeft <= 0){
+            monsterLeft += 2;
+        }
+    }
+
+}
+
+setTimeout(monsterMove, 5000);
