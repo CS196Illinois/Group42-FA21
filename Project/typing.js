bdfc35ad693b7675f43d4392ae1f525adc2100cb
sethueapen <sethueapen@gmail.com>
2021-11-15 14:39:55 -0600

    created responsive gamepad with four words that changes the position of the player and prevents wall clipping

--- Project/typing.js
+++ Project/typing.js
@@ -1,5 +1,8 @@
-const API_URL = "https://random-word-api.herokuapp.com/word?number=10"
-const quoteDisplayElement = document.getElementById('quoteDisplay')
+const API_URL = "https://random-word-api.herokuapp.com/word?number=20"
+const quoteDisplayElementN = document.getElementById('quoteDisplayN')
+const quoteDisplayElementW = document.getElementById('quoteDisplayW')
+const quoteDisplayElementS = document.getElementById('quoteDisplayS')
+const quoteDisplayElementE = document.getElementById('quoteDisplayE')
 const quoteInputElement = document.getElementById('quoteInput')
 
 var wordQueue = []
@@ -10,34 +13,97 @@ function getRandomWord() {
   .then(response => response.json())
 }
 
+function checkGameConditons() {}
+
+function updateMap(direction){
+    var changed = false
+    var temp = playerCords
+    if (direction == 1) {
+        if ((playerCords[0] - 1) >= 0 && maze[playerCords[0] - 1][playerCords[1]] == 0) {
+            playerCords = [playerCords[0] - 1, playerCords[1]]
+            changed = true
+        }
+    } else if (direction == 2) {
+        if ((playerCords[1] - 1) >= 0 && maze[playerCords[0]][playerCords[1] - 1] == 0) {
+            playerCords = [playerCords[0], playerCords[1] - 1]
+            changed = true
+        }
+    } else if (direction == 3) {
+        if ((playerCords[0] + 1) >= 0 && maze[playerCords[0] + 1][playerCords[1]] == 0) {
+            playerCords = [playerCords[0] + 1, playerCords[1]]
+            changed = true
+        }
+    } else if (direction == 4) {
+        if ((playerCords[1] + 1) >= 0 && maze[playerCords[0]][playerCords[1] + 1] == 0) {
+            playerCords = [playerCords[0], playerCords[1] + 1]
+            changed = true
+        }
+    }
+    if (changed) {
+        maze[playerCords[0]][playerCords[1]] = 2
+        maze[temp[0]][temp[1]] = 0
+    }
+    return changed
+}
+
+function printMap() {
+    console.table(maze)
+}
+
 async function getNewWord() {
-    var quote
-    console.log(queueCounter)
-    if (queueCounter >= (wordQueue.length - 3)) {
+    printMap()
+    var quoteN
+    var quoteW
+    var quoteS
+    var quoteE
+    if (queueCounter >= (wordQueue.length - 8)) {
         wordQueue = await getRandomWord()
         queueCounter = 0
-        quote = wordQueue[queueCounter]
-        queueCounter++
-    } else {
-        quote = wordQueue[queueCounter]
-        queueCounter++
-    }
-    quoteDisplayElement.innerHTML = ''
-    quote.split('').forEach(character => {
+    }    
+    quoteN = wordQueue[queueCounter]
+    queueCounter++
+    quoteW = wordQueue[queueCounter]
+    queueCounter++
+    quoteS = wordQueue[queueCounter]
+    queueCounter++
+    quoteE = wordQueue[queueCounter]
+    queueCounter++
+    quoteDisplayElementN.innerHTML = ''
+    quoteDisplayElementW.innerHTML = ''
+    quoteDisplayElementS.innerHTML = ''
+    quoteDisplayElementE.innerHTML = ''
+    quoteN.split('').forEach(character => {
         const characterSpan = document.createElement('span')
         characterSpan.innerText = character
-        quoteDisplayElement.appendChild(characterSpan)
+        quoteDisplayElementN.appendChild(characterSpan)
+    });
+    quoteW.split('').forEach(character => {
+        const characterSpan = document.createElement('span')
+        characterSpan.innerText = character
+        quoteDisplayElementW.appendChild(characterSpan)
+    });
+    quoteS.split('').forEach(character => {
+        const characterSpan = document.createElement('span')
+        characterSpan.innerText = character
+        quoteDisplayElementS.appendChild(characterSpan)
+    });
+    quoteE.split('').forEach(character => {
+        const characterSpan = document.createElement('span')
+        characterSpan.innerText = character
+        quoteDisplayElementE.appendChild(characterSpan)
     });
     quoteInputElement.value = null
 }
 
 quoteInputElement.addEventListener('input', () => {
-    const arrayQuote = quoteDisplayElement.querySelectorAll('span')
+    const arrayQuoteN = quoteDisplayElementN.querySelectorAll('span')
+    const arrayQuoteW = quoteDisplayElementW.querySelectorAll('span')
+    const arrayQuoteS = quoteDisplayElementS.querySelectorAll('span')
+    const arrayQuoteE = quoteDisplayElementE.querySelectorAll('span')
     const arrayValue = quoteInputElement.value.split('')
 
     var correct = true
-
-    arrayQuote.forEach((characterSpan, index) => {
+    arrayQuoteN.forEach((characterSpan, index) => {
         const character = arrayValue[index]
         if (character == null) {
             characterSpan.classList.remove('correct')
@@ -52,7 +118,71 @@ quoteInputElement.addEventListener('input', () => {
             correct = false
         }
     })
-    if (correct) getNewWord()
+    if (correct) {
+        updateMap(1)
+        getNewWord()
+    }
+    var correct = true
+    arrayQuoteW.forEach((characterSpan, index) => {
+        const character = arrayValue[index]
+        if (character == null) {
+            characterSpan.classList.remove('correct')
+            characterSpan.classList.remove('incorrect')
+            correct = false
+        } else if (character === characterSpan.innerText) {
+            characterSpan.classList.add('correct')
+            characterSpan.classList.remove('incorrect')
+        } else {
+            characterSpan.classList.add('incorrect')
+            characterSpan.classList.remove('correct')
+            correct = false
+        }
+    })
+    if (correct) {
+        updateMap(2)
+        getNewWord()
+    }
+    var correct = true
+    arrayQuoteS.forEach((characterSpan, index) => {
+        const character = arrayValue[index]
+        if (character == null) {
+            characterSpan.classList.remove('correct')
+            characterSpan.classList.remove('incorrect')
+            correct = false
+        } else if (character === characterSpan.innerText) {
+            characterSpan.classList.add('correct')
+            characterSpan.classList.remove('incorrect')
+        } else {
+            characterSpan.classList.add('incorrect')
+            characterSpan.classList.remove('correct')
+            correct = false
+        }
+    })
+    if (correct) {
+        updateMap(3)
+        getNewWord()
+    }
+    var correct = true
+    arrayQuoteE.forEach((characterSpan, index) => {
+        const character = arrayValue[index]
+        if (character == null) {
+            characterSpan.classList.remove('correct')
+            characterSpan.classList.remove('incorrect')
+            correct = false
+        } else if (character === characterSpan.innerText) {
+            characterSpan.classList.add('correct')
+            characterSpan.classList.remove('incorrect')
+        } else {
+            characterSpan.classList.add('incorrect')
+            characterSpan.classList.remove('correct')
+            correct = false
+        }
+    })
+    if (correct) {
+        updateMap(4)
+        getNewWord()
+    }
 })
 
+updateMap(2)
 getNewWord()
